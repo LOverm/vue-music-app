23dbff323802a86cf4ca63b19bbc35a6be94f2f0
KeenKing <969172689@qq.com>
2018-10-29 00:30:43 +0800

    10.29

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,7 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail">
-    </div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
@@ -9,9 +8,21 @@
 import {mapGetters} from 'vuex';
 import { getSingerDetail } from 'api/singer';
 import { ERR_OK } from 'api/config';
-
+import { createSong } from '../../common/js/song.js';
+import MusicList from '../music-list/music-list';
 export default {
+  data() {
+    return {
+      songs: []
+    };
+  },
   computed: {
+    title() {
+      return this.singer.name;
+    },
+    bgImage() {
+      return this.singer.avatar;
+    },
     ...mapGetters([
       'singer'
     ])
@@ -27,27 +38,31 @@ export default {
       }
       getSingerDetail(this.singer.id).then((res) => {
         if (res.code === ERR_OK) {
-          console.log(res.data.list);
+          this.songs = this._normalizeSongs(res.data.list);
         }
       });
+    },
+    _normalizeSongs(list) {
+      let ret = [];
+      list.forEach((item) => {
+        let {musicData} = item;
+        if (musicData.songid && musicData.albumid) {
+          ret.push(createSong(musicData));
+        }
+      });
+      return ret;
     }
+  },
+  components: {
+    MusicList
   }
 };
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-@import '../../common/stylus/variable'
-.singer-detail
-  position fixed
-  z-index 100
-  top 0
-  left 0
-  right 0
-  bottom 0
-  background  $color-background
-
-.slide-enter-active, .slide-leave-active
-  transition all 0.3s
-.slide-enter, .slide-leave-to
-  transform translate3d(100%, 0, 0)
+  @import '../../common/stylus/variable'
+  .slide-enter-active, .slide-leave-active
+    transition all 0.3s
+  .slide-enter, .slide-leave-to
+    transform translate3d(100%, 0, 0)
 </style>
