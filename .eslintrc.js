c525d17c11059a6ca3fd360d95fb5a3303168aed
KeenKing <969172689@qq.com>
2018-11-29 13:45:49 +0800

    11.29

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,34 +6,35 @@ module.exports = {
     parser: 'babel-eslint'
   },
   env: {
-    browser: true,
+    browser: true
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue'],
   // add your custom rules here
   rules: {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'semi': ['error', 'always'],
+    semi: ['error', 'always'],
     'space-before-function-paren': 0,
-    'indent': 0,
+    indent: 0,
     'elo-last': 0,
-    "no-unused-vars": [0, {
-      // 允许声明未使用变量
-      "vars": "local",
-      // 参数不检查
-      "args": "none"
-    }],
+    'no-unused-vars': [
+      0,
+      {
+        // 允许声明未使用变量
+        vars: 'local',
+        // 参数不检查
+        args: 'none'
+      }
+    ]
   }
-}
+};
